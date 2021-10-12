27647451f5568d435cf1193bda7cf9f5bd15566d
Iakovlev-Dev <81575417+Iakovlev-Dev@users.noreply.github.com>
2021-10-12 22:01:35 +0300

    Add files via upload

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,22 @@
+import {multiply} from '../index';
+
+describe("test multiply 1", () => {
+  it('multiply 1 * 0 to equal 0', () => {
+    const result = multiply(1, 0);
+    expect(result).toBe(0);
+  }),
+  xit('multiply 1 * 0 to equal 1', () => {
+    const result = multiply(1, 0);
+    expect(result).toBe(1);
+  })
+});
+describe("test multiply 2", () => {
+  it('multiply 1 * 0 to equal 0', () => {
+    const result = multiply(1, 0);
+    expect(result).toBe(0);
+  }),
+  xit('multiply 1 * 0 to equal 1', () => {
+    const result = multiply(1, 0);
+    expect(result).toBe(1);
+  })
+});
